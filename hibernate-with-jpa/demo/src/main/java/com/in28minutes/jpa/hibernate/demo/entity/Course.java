2e273580c1ce7cb4594e2889917b130a11461b74
Angel Aguilera <angel.aguilera@gmail.com>
2026-03-24 13:10:26 +0100

    Create Course JPA entity

---
+++ hibernate-with-jpa/demo/src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Course.java
@@ -0,0 +1,42 @@
+package com.in28minutes.jpa.hibernate.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String name;
+
+	/**
+	 * Default no-argument constructor is explicitly required because we added
+	 * another constructor with 'name' argument.
+	 */
+	protected Course() {
+	}
+
+	public Course(String name) {
+		this.setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * We only define a 'getter' for property id (no setter).
+	 */
+	public Long getId() {
+		return id;
+	}
+
+}
