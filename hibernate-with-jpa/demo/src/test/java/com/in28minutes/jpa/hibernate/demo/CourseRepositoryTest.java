5b6eed7bc0fd29be6da7e36a39709a0eedb74ade
Angel Aguilera <angel.aguilera@gmail.com>
2026-03-27 12:21:04 +0100

    add test for CourseRepository.save() ('update' version)

--- hibernate-with-jpa/demo/src/test/java/com/in28minutes/jpa/hibernate/demo/CourseRepositoryTest.java
+++ hibernate-with-jpa/demo/src/test/java/com/in28minutes/jpa/hibernate/demo/CourseRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
+import com.in28minutes.jpa.hibernate.demo.entity.Course;
 import com.in28minutes.jpa.hibernate.demo.repository.CourseRepository;
 
 @SpringBootTest
@@ -24,13 +25,27 @@ class CourseRepositoryTest {
 
 	/**
 	 * This test changes the database status, so we use the {@link DirtiesContext}
-	 * annotation to make Spring test runner to reset the database status after this
-	 * test is run, thus avoiding other tests which depend on the same data to fail.
+	 * annotation.
 	 */
 	@Test
 	@DirtiesContext
 	void deleteById_basic() {
 		repository.deleteById(10002L);
 		assertNull(repository.findById(10002L));
+	}
+
+	@Test
+	@DirtiesContext
+	void save_basic() {
+		// get course details
+		var course = repository.findById(10001L);
+
+		// update details
+		course.setName("JPA in 50 steps - Updated");
+		repository.save(course);
+
+		// check updated details
+		var course1 = repository.findById(10001L);
+		assertEquals("JPA in 50 steps - Updated", course1.getName());
 	}
 }
