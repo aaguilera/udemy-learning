6207d2ca14fc9713b71afafa4e5ada9f72718371
Angel Aguilera <angel.aguilera@gmail.com>
2026-03-27 11:45:15 +0100

    add test for CourseRepository.deleteById

--- hibernate-with-jpa/demo/src/test/java/com/in28minutes/jpa/hibernate/demo/CourseRepositoryTest.java
+++ hibernate-with-jpa/demo/src/test/java/com/in28minutes/jpa/hibernate/demo/CourseRepositoryTest.java
@@ -1,10 +1,12 @@
 package com.in28minutes.jpa.hibernate.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import com.in28minutes.jpa.hibernate.demo.repository.CourseRepository;
 
@@ -20,4 +22,15 @@ class CourseRepositoryTest {
 		assertEquals("JPA in 50 steps", course.getName());
 	}
 
+	/**
+	 * This test changes the database status, so we use the {@link DirtiesContext}
+	 * annotation to make Spring test runner to reset the database status after this
+	 * test is run, thus avoiding other tests which depend on the same data to fail.
+	 */
+	@Test
+	@DirtiesContext
+	void deleteById_basic() {
+		repository.deleteById(10002L);
+		assertNull(repository.findById(10002L));
+	}
 }
